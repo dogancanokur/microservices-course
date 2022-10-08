d554bf10c46467ceccb4dbebc9a16f245c286f78
dogancan.okur <dogancan.okur@gmail.com>
2022-10-08 03:28:41 +0300

    product service init

---
+++ src/product-service/src/test/java/net/okur/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.okur.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
