198686b017674f789765f14ce237fab82a7998dd
dogancan.okur <dogancan.okur@gmail.com>
2022-12-04 03:44:21 +0300

    gateway service - spring security configs -authorization -authentication -cors

--- src/gatewayService/src/main/java/net/okur/gatewayservice/GatewayServiceApplication.java
+++ src/gatewayService/src/main/java/net/okur/gatewayservice/GatewayServiceApplication.java
@@ -2,9 +2,6 @@ package net.okur.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -13,8 +10,4 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
--- src/gatewayService/src/main/java/net/okur/gatewayservice/security/CustomUserDetailsService.java
+++ src/gatewayService/src/main/java/net/okur/gatewayservice/security/CustomUserDetailsService.java
@@ -14,12 +14,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
-    private final IUserService userService;
-
     @Autowired
-    public CustomUserDetailsService(IUserService userService) {
-        this.userService = userService;
-    }
+    private IUserService userService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
